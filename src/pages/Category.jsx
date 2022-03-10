fbd149db2cd65b65e1e6ea7f4852abd58f86ebb3
William Gao <william_gao0206@hotmail.com>
2022-03-10 15:19:27 +0800

    Get listings

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,6 +1,52 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+  setDoc,
+  getDoc,
+  doc,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
 
 function Category() {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const params = useParams();
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        //Fetch a reference
+        const listingsRef = collection(db, "listing");
+
+        //create a query
+        const q = query(
+          listingsRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+        //Excute Query
+        const querySnap = await getDocs(q);
+
+        let listings = [];
+        querySnap.forEach(async (doc) => {
+          console.log(doc);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchListings();
+  });
+
   return <div>Category</div>;
 }
 
